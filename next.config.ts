e80d8711105312dbaf6b68b43e8f32012216e543
Halim Choukani <halim.choukani@gmail.com>
2025-11-01 00:09:32 +0100

    feat: update image configuration to use remotePatterns for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,31 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "s.alicdn.com",
-      "dropinblog.net",
-      "www.britsoc.co.uk",
-      "cdncn.goodao.net",
-      "cdn-icons-png.freepik.com",
-      "mk-media.mytek.tn",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s.alicdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "dropinblog.net",
+      },
+      {
+        protocol: "https",
+        hostname: "www.britsoc.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "cdncn.goodao.net",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mk-media.mytek.tn",
+      },
     ],
   },
   eslint: {
